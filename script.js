8a48aba0d086bfb57cfc7664c4405318f94c8588
Ashish Pandey <writeashish22@gmail.com>
2025-07-14 21:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let btn = document.getElementByID("openModal")
+let outerDiv = document.querySelector(".modal")
+btn.addEventlistener("click",()=>{
+
+})
